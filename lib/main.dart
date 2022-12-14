8eb514e4166a89e29d7996964415ca0b5cebb308
Tw2700 <towaj2002@icloud.com>
2022-12-14 10:06:00 +0900

    コミット

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,105 +13,112 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'ポートフォリオ',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const portfolio(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class portfolio extends StatefulWidget {
+  const portfolio({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<portfolio> createState() => _portfolioState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _portfolioState extends State<portfolio> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      drawer: const Drawer(),
+      body: Stack(
+        children: [
+          Positioned(
+            top: -500,
+            left: -70,
+            child: Transform.rotate(
+              angle: pi / 2.7,
+              child: Material(
+                color: const Color.fromARGB(255, 241, 241, 241),
+                child: Container(
+                  color: const Color.fromARGB(255, 241, 241, 241),
+                  width: 400,
+                  height: 900,
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 30),
+            child: Material(
+              clipBehavior: Clip.antiAlias,
+              borderRadius: BorderRadius.circular(32),
+              color: const Color.fromARGB(255, 241, 241, 241),
+              child: IconButton(
+                onPressed: () {
+                  print('tap');
+                },
+                icon: const Icon(Icons.menu_sharp),
+              ),
             ),
-          ],
-        ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 32, left: 80),
+            child: const Text('Menu'),
+          ),
+          Center(
+            child: Column(
+              children: [
+                const SizedBox(height: 250),
+                Container(),
+                const Text(
+                  'TOWA ISHIKAWA',
+                  style: TextStyle(
+                    fontSize: 40,
+                    letterSpacing: 3.0,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const Text(
+                  'TOWA ISHIKAWA is an Art Director/Designer based.',
+                  style: TextStyle(
+                    fontSize: 25,
+                    letterSpacing: 3.0,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'He seeks and creates a more prominent design,',
+                  style: TextStyle(
+                    fontSize: 25,
+                    letterSpacing: 3.0,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'together with his proficient partners.',
+                  style: TextStyle(
+                    fontSize: 25,
+                    letterSpacing: 3.0,
+                  ),
+                ),
+                const SizedBox(
+                  height: 80,
+                ),
+                const Text('Sclool'),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.keyboard_double_arrow_down_rounded),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
